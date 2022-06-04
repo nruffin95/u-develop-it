e9f3670eba2c4f82868aa37086b7936888a3ebcc
nruffin95 <nruffin95@gmail.com>
2022-06-04 07:47:10 -0400

    Added msql rows

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,24 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Mysql username,
+        user: 'root',
+        // Your Mysql password
+        password: '#Granger33',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
